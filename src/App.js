fbda826a2a1a37a85507f169ebefcc1cd0620c89
Robert Patrick Jordan Sazon <rpjsazon@gmail.com>
2023-03-15 16:12:07 +0000

    added home page layout with react bootstrap.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { Container, Row, Col } from 'react-bootstrap';
 import './App.css';
+import SideBar from './components/SideBar';
+import Form from './components/Form';
+import GenerateCard from './components/GeneratedCard';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Container>
+        <Header />
+        <Row>
+          <Col xs={3}><SideBar /></Col>
+          <Col xs={5}><Form /></Col>
+          <Col xs={4}><GenerateCard /></Col>
+        </Row>
+      </Container>
     </div>
   );
 }
---
+++ src/components/GeneratedCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function GenerateCard() {
+  return (
+    <div>
+      Generate Card
+    </div>
+  )
+}
+
+export default GenerateCard;
