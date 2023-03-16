041e0aeb0231dcb946d3e221de1cb6d8a5ce922e
Kejvin Pashaj <kevin.952011@gmail.com>
2023-03-16 17:03:57 +0000

    added google maps component

--- src/index.js
+++ src/index.js
@@ -4,10 +4,5 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
