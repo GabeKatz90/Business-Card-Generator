d3af4c77c0bdbf30e2b78dce78739a7e24e03e3d
Gabriel Katz <gabrielkatz@Gabriels-MacBook-Pro.local>
2023-03-23 12:00:33 +0000

    onelasttime

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,5 @@
 import Grid from '@mui/material/Grid'; // Grid version 1
-import Grid2 from '@mui/material/Unstable_Grid2'; // Grid version 2
+// import Grid2 from '@mui/material/Unstable_Grid2'; // Grid version 2
 
 function About (){
   
