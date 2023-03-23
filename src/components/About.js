831864cb343e147b7720e87e28f131575bc3c803
Gabriel Katz <gabrielkatz@Gabriels-MacBook-Pro.local>
2023-03-23 13:58:13 +0000

    onelasttimehopefully

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,64 @@
-import Grid from '@mui/material/Grid'; // Grid version 1
-// import Grid2 from '@mui/material/Unstable_Grid2'; // Grid version 2
+// import Grid from '@mui/material/Grid'; // Grid version 1
+// // import Grid2 from '@mui/material/Unstable_Grid2'; // Grid version 2
 
-function About (){
+// function About (){
   
-<Grid container spacing={2} minHeight={160}>
-  <Grid xs display="flex" justifyContent="center" alignItems="center">
-    {/* <Avatar src="/static/images/avatar/1.jpg" /> */}
-  </Grid>
-  <Grid display="flex" justifyContent="center" alignItems="center">
-    {/* <Avatar src="/static/images/avatar/2.jpg" /> */}
-  </Grid>
-  <Grid xs display="flex" justifyContent="center" alignItems="center">
-    {/* <Avatar src="/static/images/avatar/3.jpg" /> */}
-  </Grid>
-</Grid>
+// <Grid container spacing={2} minHeight={160}>
+//   <Grid xs display="flex" justifyContent="center" alignItems="center">
+//     {/* <Avatar src="/static/images/avatar/1.jpg" /> */}
+//   </Grid>
+//   <Grid display="flex" justifyContent="center" alignItems="center">
+//     {/* <Avatar src="/static/images/avatar/2.jpg" /> */}
+//   </Grid>
+//   <Grid xs display="flex" justifyContent="center" alignItems="center">
+//     {/* <Avatar src="/static/images/avatar/3.jpg" /> */}
+//   </Grid>
+// </Grid>
+// }
+// export default About;
+
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import { Row, Col, Container } from "react-bootstrap";
+
+function Contact () {
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col>
+          <Card style={{ height: '20rem' }}>
+          <Card.Title>Generate Business Card Company Limited</Card.Title>
+          <Card.Text>
+          
+          Start Up Company Based In England
+         
+        </Card.Text>
+        
+      <Card.Body>
+      
+        <Card.Text>
+
+        
+          90 The Crescent ,London W24 8CS
+          England
+          
+        </Card.Text>
+        
+        
+     
+        
+      
+      </Card.Body>
+      <img src="https://www.google.com/maps/vt/data=KZzZvi4gOKoZ44FLEd_UR_fAs3Niqyp8YefF_AMLtJ2QAwcsk8kZHabDSAvIwV6qUX2PaKCb7QZIIgzyt4VpU6j6MC6DODFKdxqfLITq0AkmZOKqEYXBjt_LNcdCOhfita7X5SpT33kIGF9VnMNMyoAsal9Qq47L5nM7xa837gFaandvzUPYLY6NB4Ar8yA56SRIpUgBy_fGyGYndSenal7L7JwfqMzBj8fdU5_J8lF5uB1tDbqoBxaVk198zHIGBREw4DStoQ" alt="React Image" />
+      <Button onClick={() => window.open("", '_blank')}>Email US!</Button>
+    </Card>
+          </Col>
+  
+        </Row>
+      </Container>
+    </>
+  );
 }
-export default About;
+
+export default Contact;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,35 +1,68 @@
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import { Row, Col, Container } from "react-bootstrap";
 
 function Contact () {
   return (
     <>
-      {[
-        'Primary',
-        'Secondary',
-        'Success',
-        'Danger',
-        'Warning',
-        'Info',
-        'Light',
-        'Dark',
-      ].map((variant) => (
-        <Card
-          bg={variant.toLowerCase()}
-          key={variant}
-          text={variant.toLowerCase() === 'light' ? 'dark' : 'white'}
-          style={{ width: '18rem' }}
-          className="mb-2"
-        >
-          <Card.Header>Header</Card.Header>
-          <Card.Body>
-            <Card.Title>{variant} Card Title </Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      ))}
+      <Container>
+        <Row>
+          <Col>
+          <Card style={{ width: '20rem' }}>
+          <Card.Title>Kejvin Pashaj</Card.Title>
+
+      <Card.Body>
+        
+        <Card.Text>
+          Junior Developer
+          London, England
+        </Card.Text>
+        
+     
+        
+      
+      </Card.Body>
+      <Button onClick={() => window.open("https://www.linkedin.com/in/kevpash/", '_blank')}>LinkenIn</Button>
+    </Card>
+          </Col>
+          <Col>
+          <Card style={{ width: '20rem' }}>
+          <Card.Title>Robert Patrick Sazon</Card.Title>
+
+      <Card.Body>
+
+        
+        <Card.Text>
+          Junior Developer
+          
+          Bath,England
+        </Card.Text>
+  
+        
+      </Card.Body>
+      <Button onClick={() => window.open("https://www.linkedin.com/in/rjpsazon/", '_blank')}>LinkenIn</Button>
+    </Card>
+          </Col>
+
+          <Col>
+          <Card style={{ width: '20rem' }}>
+          <Card.Title>Gabriels Katz</Card.Title>
+
+      <Card.Body>
+        
+        <Card.Text>
+         Junior Developer
+
+         London,England
+        </Card.Text>
+        
+      </Card.Body>
+      <Button onClick={() => window.open("https://www.linkedin.com/in/gabkatz90/", '_blank')}>LinkenIn</Button>
+    </Card>
+          </Col>
+          
+        </Row>
+      </Container>
     </>
   );
 }
